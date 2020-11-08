495146d8aaa3717acd36225483eaff223e0fb99c
stanusiucristina <cristina.stanusiu@coreai.ai>
2020-11-08 21:33:51 +0200

    complete TransactionManager

--- src/main/java/server/TransactionManager.java
+++ src/main/java/server/TransactionManager.java
@@ -2,25 +2,41 @@ package server;
 
 import utils.Order;
 import utils.OrderBook;
-
 import java.util.Queue;
 
 public class TransactionManager {
     private OrderBook orderManager = new OrderBook();
 
-    public void markCompleteOrder(Order order){
-        orderManager.removeSellOrder(order);
-        orderManager.removeBuyOrder(order);
-        orderManager.putHistoryOrderIfAbsent(order);
+    public void markCompleteOrder(Order sellOrder, Order buyOrder){
+        orderManager.removeSellOrder(sellOrder);
+        orderManager.removeBuyOrder(buyOrder);
+        orderManager.putHistoryOrderIfAbsent(sellOrder);
+        orderManager.putHistoryOrderIfAbsent(buyOrder);
     }
 
     public void checkPriceMatch(){
         Queue<Order> buyOrders = orderManager.getBuyOrders();
         Queue<Order> sellOrders = orderManager.getSellOrders();
 
-//        for(int i = 0;i< sellOrders.size();i++){
-//        }
-
+        for(Order sellOrder: sellOrders){
+            for(Order buyOrder: buyOrders){
+                if(sellOrder.getPricePerAction() == buyOrder.getPricePerAction()){
+                    if(sellOrder.getSharesNumber() == buyOrder.getSharesNumber()){
+                        this.markCompleteOrder(sellOrder,buyOrder);
+                    }
+                    if(sellOrder.getSharesNumber() < buyOrder.getSharesNumber()){
+                        orderManager.putHistoryOrderIfAbsent(sellOrder);
+                        orderManager.removeSellOrder(sellOrder);
+                        buyOrder.setSharesNumber(buyOrder.getSharesNumber() - sellOrder.getSharesNumber());
+                    }
+                    else{
+                        orderManager.putHistoryOrderIfAbsent(buyOrder);
+                        orderManager.removeBuyOrder(buyOrder);
+                        sellOrder.setSharesNumber(sellOrder.getSharesNumber() - buyOrder.getSharesNumber());
+                    }
+                }
+            }
+        }
     }
 
 }
