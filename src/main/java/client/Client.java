28564431b3876a292b2002ba2475a7963a5fa03f
stanusiucristina <cristina.stanusiu@coreai.ai>
2020-11-09 19:59:11 +0200

    local cris

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -63,8 +63,8 @@ public class Client {
 
         Order sellOrder_1 = new Order(1000, 24, seller_1.clientId, Order.OrderType.SELL);
         seller_1.sendOrder(sellOrder_1);
-//        sellOrder_1.setPricePerAction(25);
-//        seller_1.sendOrder(sellOrder_1);
+        sellOrder_1.setPricePerAction(25);
+        seller_1.sendOrder(sellOrder_1);
 
 //        Order sellOrder_2 = new Order(80, 24, seller_1.clientId, Order.OrderType.SELL);
 //        seller_1.sendOrder(sellOrder_2);
